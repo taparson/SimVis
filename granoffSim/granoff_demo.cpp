22ddbea6bfece51a5c6e5af32efae58678438d89
taparson <timothy_parsons@brown.edu>
2015-05-14 14:56:02 -0400

    meow

--- granoffSim/granoff_demo.cpp
+++ granoffSim/granoff_demo.cpp
@@ -63,7 +63,6 @@ public:
   virtual ~MyVRApp() {delete _tracheaApp;}
 
   void intialize()  {
-    _tracheaApp->initialize();
     std::string line;
     std::ifstream myfile ("background.obl",std::ifstream::in);
     if (myfile.is_open())
@@ -246,7 +245,7 @@ public:
     glStencilFunc(GL_EQUAL, 1, 0xFF);
    
     glColor4f(1.0f,1.0f,1.0f,.4f);
-    _tracheaApp->doGraphics();
+    _tracheaApp->doGraphics(rd);
     glColor4f(1.0f,1.0f,1.0f,.4f);
     for(int i = 0; i < _stencil.size(); i++)  {
       _stencil[i].draw();
